09afc65e0201881a6296345bbd011114841e4dfa
Guilherme Lucas <guilherme.evandf@gmail.com>
2020-07-14 19:46:49 -0300

    User Entity and Resource

---
+++ src/main/java/com/course/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.course.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.course.course.entities.User;
+
+@RestController
+@RequestMapping( value = "/users" )
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Guilherme", "guilherme@gmail.com", "999622175", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
